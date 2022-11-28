46873bde22a7b1d25644b98a0aa650268f614d83
Will Acton <will@willacton.com>
2022-11-28 08:57:01 -0800

    implement with-gen and specize protocol

--- src/helix/spec/alpha.cljs
+++ src/helix/spec/alpha.cljs
@@ -9,27 +9,32 @@
 (defn props
   ([spec] (props spec nil))
   ([spec gfn]
-   (let [spec (delay (#'s/specize spec))]
+   (let [*spec (delay (#'s/specize spec))]
      (reify
+       s/Specize
+       (specize* [this] this)
+       (specize* [this _] this)
+
        s/Spec
        (conform* [_ x]
          (try
-           (let [m (into {} (helix/extract-cljs-props x))]
-             (s/conform* @spec m))
+           (let [m (into {} (helix/extract-cljs-props x))] ; ensure its a map
+             (s/conform* @*spec m))
            (catch js/Object _e
              ::invalid)))
        (unform* [_ m]
-         (let [x (s/unform* @spec m)]
+         (let [x (s/unform* @*spec m)]
            (helix.impl.props/-props x)))
        (explain* [_ path via in x]
          (if-not (object? x)
            [{:path path :pred `object? :val x :via via :in in}]
-           (s/explain* @spec path via in (into {} (helix/extract-cljs-props x)))))
+           ;; TODO should catch and explain error while extracting
+           (s/explain* @*spec path via in (into {} (helix/extract-cljs-props x)))))
        (gen* [_ overrides path rmap]
          (if gfn
            (gen/fmap helix.impl.props/-props (gfn))
-           (gen/fmap helix.impl.props/-props (s/gen* @spec overrides path rmap))))
+           (gen/fmap helix.impl.props/-props (s/gen* @*spec overrides path rmap))))
        (with-gen* [_ gfn]
-         (throw (ex-info "Not implemented" {})))
+         (props spec gfn))
        (describe* [_]
-         [`props @spec])))))
+         [`props @*spec])))))
